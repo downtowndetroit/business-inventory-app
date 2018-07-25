e954a3128b3ee23c7d4c5816c34aae6343b1cc5a
tianxie1995 <13590388977@163.com>
2018-07-25 18:24:25 -0400

    july 25

--- README.md
+++ README.md
@@ -8,9 +8,15 @@ Online Server, Detroit Open Data, and ORBIS
 Database. 
 
 ## What it can do ?
+### Parcel view
 By simply clicking parcel of interest or typing parcel address,
  users can see:
 1. All information about the parcel in a pretty 
 table format.
 2. List of businesses inside that parcel.
 3. Attribute of the building that parcel belongs to. 
+
+### Restaurant view
+### Parking garage view 
+### MoGo view 
+###...
--- js/map.js
+++ js/map.js
@@ -1,17 +1,34 @@
+/* This is the script to create ArcGIS map*/
 require([
         "esri/map",
         "esri/layers/FeatureLayer",
-        "dojo/domReady!"
+        "esri/symbols/SimpleFillSymbol",
+        "esri/symbols/SimpleLineSymbol",
+        "esri/renderers/SimpleRenderer",
+        "esri/Color",
+        "esri/tasks/query",
+        "esri/graphic",
+        "dojo/domReady!",
+        "dojo/ready",
+        "dojo/on",
     ],
     function(
         Map,
-        FeatureLayer
+        FeatureLayer,
+        SimpleFillSymbol,
+        SimpleLineSymbol,
+        SimpleRenderer,
+        Color,
+        Query,
+        Graphic,
+        ready,
+        on
     ) {
 
         var map = new Map("map", {
             basemap: "hybrid",
             center: [-83.052738,42.335301],
-            zoom: 17
+            zoom: 16
         });
 
         /****************************************************************
@@ -21,8 +38,68 @@ require([
          ***************************************************************/
 
             // Carbon storage of trees in Warren Wilson College.
-        var featureLayer = new FeatureLayer("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/arcgis/rest/services/May2018Parcels/FeatureServer/0");
+        var ORBIS = new FeatureLayer("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/arcgis/rest/services/ORBIS_BIZ/FeatureServer/0",{
+                mode: FeatureLayer.MODE_ONDEMAND,
+                outFields: ['*'],
+                opacity: 0.2,
+                visible: false,
+                orderByFields: ["Field1"]
+            });
+        var PARCEL = new FeatureLayer("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/arcgis/rest/services/May2018Parcels/FeatureServer/0",{
+            mode: FeatureLayer.MODE_ONDEMAND,
+            outFields: ['*'],
+            opacity: 0.8,
+            visible: true,
+            orderByFields: ["objectid"]
+        });
+        var parcel_symbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+            new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
+                new Color('white'), 1),
+            new Color('lightblue'));
+        var renderer = new SimpleRenderer(parcel_symbol);
+        PARCEL.setRenderer(renderer);
 
-        map.addLayer(featureLayer);
+        var BUILDING = new FeatureLayer("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/ArcGIS/rest/services/BUILDING_BIZ/FeatureServer/0",{
+            mode: FeatureLayer.MODE_ONDEMAND,
+            outFields: ['*'],
+            opacity: 0.2,
+            visible: false,
+            orderByFields: ["FID"]
+        });
+        // apply the selection symbol for the layer
+        var selectionSymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+            new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
+                new Color('lightgrey'), 2),
+            new Color('red'));
+        PARCEL.setSelectionSymbol(selectionSymbol);
+        // setting click event, when click parcel, graphic will change
+        PARCEL.on("click", function(evt) {
+            var idProperty = PARCEL.objectIdField;
+            var feature, featureId, query;
+
+            if (evt.graphic && evt.graphic.attributes && evt.graphic.attributes[idProperty]) {
+                feature = evt.graphic;
+                featureId = feature.attributes[idProperty];
+
+                query = new Query();
+                query.returnGeometry = false;
+                query.objectIds = [featureId];
+                query.where = "1=1";
+
+                PARCEL.selectFeatures(query, FeatureLayer.SELECTION_NEW, function (feature) {
+                    var attribute = feature[0]["attributes"];
+                    var parcelnum = feature[0]["attributes"]["parcelnum"];
+                    console.log("arrtribute:"+attribute);
+                    console.log("parcel number: "+parcelnum);
+                });
+            }
+        });
+
+
+
+        map.addLayer(ORBIS);
+        map.addLayer(PARCEL);
+        map.addLayer(BUILDING);
+
 
     });
