a7e6cd668ce7afaead4f74726476e58e6fa76a5c
tianxie1995 <13590388977@163.com>
2018-07-26 17:00:55 -0400

    add new features
    retrive business and building data,
    add autocomplete address input box;

--- js/map.js
+++ js/map.js
@@ -7,6 +7,7 @@ require([
         "esri/renderers/SimpleRenderer",
         "esri/Color",
         "esri/tasks/query",
+        "esri/tasks/QueryTask",
         "esri/graphic",
         "dojo/domReady!",
         "dojo/ready",
@@ -20,6 +21,7 @@ require([
         SimpleRenderer,
         Color,
         Query,
+        QueryTask,
         Graphic,
         ready,
         on
@@ -38,13 +40,23 @@ require([
          ***************************************************************/
 
             // Carbon storage of trees in Warren Wilson College.
-        var ORBIS = new FeatureLayer("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/arcgis/rest/services/ORBIS_BIZ/FeatureServer/0",{
+        /*var ORBIS = new FeatureLayer("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/arcgis/rest/services/ORBIS_BIZ/FeatureServer/0",{
                 mode: FeatureLayer.MODE_ONDEMAND,
                 outFields: ['*'],
                 opacity: 0.2,
                 visible: false,
                 orderByFields: ["Field1"]
-            });
+            });*/
+        /*var BUILDING = new FeatureLayer("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/ArcGIS/rest/services/BUILDING_BIZ/FeatureServer/0",{
+                mode: FeatureLayer.MODE_ONDEMAND,
+                outFields: ['*'],
+                opacity: 0.2,
+                visible: false,
+                orderByFields: ["FID"]
+            });*/
+        var orbis_querytask = new QueryTask("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/arcgis/rest/services/ORBIS_BIZ/FeatureServer/0");
+        var building_querytask = new QueryTask("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/ArcGIS/rest/services/BUILDING_BIZ/FeatureServer/0");
+        var parcel_querytask = new QueryTask("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/arcgis/rest/services/May2018Parcels/FeatureServer/0");
         var PARCEL = new FeatureLayer("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/arcgis/rest/services/May2018Parcels/FeatureServer/0",{
             mode: FeatureLayer.MODE_ONDEMAND,
             outFields: ['*'],
@@ -52,20 +64,26 @@ require([
             visible: true,
             orderByFields: ["objectid"]
         });
+
+        address_list = [];
+        address_query = new Query();
+        address_query.returnGeometry = false;
+        address_query.outFields = ['address'];
+        address_query.where = "1=1";
+        parcel_querytask.execute(address_query, function (results){
+            results['features'].forEach(function (feature) {
+                address_list.push(feature['attributes']['address'])
+            });
+        });
+        console.log(address_list);
+
+
         var parcel_symbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
             new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
                 new Color('white'), 1),
             new Color('#adb2b8'));
         var renderer = new SimpleRenderer(parcel_symbol);
         PARCEL.setRenderer(renderer);
-
-        var BUILDING = new FeatureLayer("https://services6.arcgis.com/kpe5MwFGvZu9ezGW/ArcGIS/rest/services/BUILDING_BIZ/FeatureServer/0",{
-            mode: FeatureLayer.MODE_ONDEMAND,
-            outFields: ['*'],
-            opacity: 0.2,
-            visible: false,
-            orderByFields: ["FID"]
-        });
         // apply the selection symbol for the layer
         var selectionSymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
             new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
@@ -89,18 +107,175 @@ require([
                 PARCEL.selectFeatures(query, FeatureLayer.SELECTION_NEW, function (feature) {
                     var attribute = feature[0]["attributes"];
                     var parcelnum = feature[0]["attributes"]["parcelnum"];
-                    console.log("arrtribute:"+attribute);
-                    console.log("parcel number: "+parcelnum);
+                    $("#business_tablebody").empty();
+                    load_parcel(attribute);
+                    load_business(parcelnum);
+                    load_building(parcelnum);
                 });
             }
         });
+        var parcel_fieldmapping = {
+            /* address+ zipcode */
+            address: 'Address',
+            BIZ: 'BIZ',
+            parcelnum: 'Parcel Number',
+            related_pa: 'Related Parcel',
+            /* owner info */
+            owner1: 'Owner Information',
+            owner_stre: 'Owner Address',
+            /* tax info */
+            taxable_st: 'Tax Information',
+            taxable_va: 'Taxable Values',
+            taxpayer: 'Tax Payer',
+            zoning: 'Zoning Code',
+            council_di: 'Council District',
+            nez: 'NEZ',
+            property_c: 'Property Class',
+            sqft: 'Square Feet',
+            frontage: 'Frontage',
+            legaldesc: 'Legal Description'
+        };
+        load_parcel = function(attributes) {
+            $("#parcel_tablebody").empty();
+            for (attribute in parcel_fieldmapping) {
+                if (attribute == 'address') {
+                    var field_name = parcel_fieldmapping[attribute];
+                    var value = attributes[attribute] + ', ' + attributes['zip_code'];
+                    $("#parcel_tablebody").append(
+                        '<tr>' +
+                        '<td class="text-left">' + field_name + '</td>' +
+                        '<td class="text-right">' + value + '</td>' +
+                        '</tr>');
+                } else if (attribute == 'owner_stre') {
+                    var field_name = parcel_fieldmapping[attribute];
+                    var value = attributes['owner_stre']+attributes['owner_city']+attributes['owner_stat']+attributes['owner_zip'];
+                    $("#parcel_tablebody").append(
+                        '<tr>' +
+                        '<td class="text-left">' + field_name + '</td>' +
+                        '<td class="text-right">' + value + '</td>' +
+                        '</tr>');
+                } else {
+                    var field_name = parcel_fieldmapping[attribute];
+                    var value = attributes[attribute];
+                    $("#parcel_tablebody").append(
+                        '<tr>' +
+                        '<td class="text-left">' + field_name + '</td>' +
+                        '<td class="text-right">' + value + '</td>' +
+                        '</tr>')
+                }
+            }
+        };
+        var business_fieldmapping = {
+            Company_na: 'Company Name',
+            Street__no: 'Address',
+            City: 'City',
+            Last_avail: 'Last Available Year',
+            NAICS_2018: 'NAICS Core Classification',
+            NAICS_2020: 'NAICS Seconday Classification',
+            NAICS_2022: 'NAICS Minor Classification',
+            Consolidat: 'Consolidation CODE',
+            Type_: 'Type of Address',
+            Telephone_: 'Phone Number'
+        };
+        load_business = function(parcelnum){
+            var  business_query;
+            business_query = new Query();
+            business_query.outFields = ["*"];
+            business_query.where =  "parcelnum='"+parcelnum+"'";
+            var business_num = 0;
+            orbis_querytask.execute(business_query, function (results) {
+                if (results['features'].length === 0) {
+                    $("#business_tablebody").append('<tr class="table-dark">' +
+                        '<td class="text-left text-danger">Business Not Found</td>' +
+                        '<td class="text-right text-danger"></td>' +
+                        '</tr>')
+                } else {
+                    results['features'].forEach(function (feature) {
+                        var attributes = feature['attributes'];
+                        business_num += 1;
+                        $("#business_tablebody").append(
+                            '<tr class="table-dark">' +
+                            '<td colspan="2">' +'<a class="text text-info" data-toggle="collapse" ' +
+                            'href="#business'+business_num+'"  aria-controls="business'+business_num+'">'+
+                            feature['attributes']['Company_na'] + '</a></td>' +
+                            '</tr>'+
+                            '</div>');
+                        $("#business_tablebody").append(
+                            '<div class="collapse" id="business'+business_num+'">'+
+                            '</div>'
+                        );
+                        for (attribute in business_fieldmapping) {
+                            if (attribute == 'Street__no') {
+                                var field_name = business_fieldmapping[attribute];
+                                var value = attributes[attribute] + ', ' + attributes['Street___1'];
+                            } else if (attribute == 'City') {
+                                var field_name = business_fieldmapping[attribute];
+                                var value = attributes[attribute] + ', ' + attributes['Postcode'];
+                            } else if (attribute == 'Telephone_') {
+                                var field_name = business_fieldmapping[attribute];
+                                var value = attributes[attribute] + ', ' + attributes['E_mail_add'];
+                            } else {
+                                var field_name = business_fieldmapping[attribute];
+                                var value = attributes[attribute];
+                            }
+                            $("#business"+business_num).append(
+                                '<tr>' +
+                                '<td class="text-left">' + field_name + '</td>' +
+                                '<td class="text-right">' + value + '</td>' +
+                                '</tr>')
+                        }
+                    });
+                }
+            });
+        };
+        var building_fieldmapping = {
+            /* address+ zipcode */
+            AKA: 'Building Name',
+            APN: 'Parcel Number',
+            BUILDING_I: 'Building ID',
+            BUILD_TYPE: 'Building Type',
+            YEAR_BUILT: 'Built Year',
+            CONDITION: 'Condition',
+            HOUSING_UN: 'Housing Units',
+            STORIES: 'Stories',
+            MEDIAN_HGT: 'Median Height',
+            RES_SQFT: 'Residential Area',
+            NONRES_SQF: 'NOT-Residential Area'
+        };
+        load_building= function(parcelnum) {
+            var building_query;
+            building_query = new Query();
+            building_query.outFields = ["*"];
+            building_query.where = "APN='" + parcelnum + "'";
+            $("#building_tablebody").empty();
+            building_querytask.execute(building_query, function (results) {
+                if (results['features'].length === 0) {
+                    $("#building_tablebody").append('<tr class="table-dark">' +
+                        '<td class="text-left text-danger">Building Not Found</td>' +
+                        '<td class="text-right text-danger"></td>' +
+                        '</tr>')
+                } else {
+                    results['features'].forEach(function (feature) {
+                        var attributes = feature['attributes'];
+                        for (attribute in building_fieldmapping) {
+                            var field_name = building_fieldmapping[attribute];
+                            var value = attributes[attribute];
+                            $("#building_tablebody").append(
+                                '<tr>' +
+                                '<td class="text-left">' + field_name + '</td>' +
+                                '<td class="text-right">' + value + '</td>' +
+                                '</tr>')
 
+                        }
+                    });
+                }
+            });
+        };
 
-
-
-        map.addLayer(ORBIS);
+        $("#parcel_detail").collapse('show');
         map.addLayer(PARCEL);
-        map.addLayer(BUILDING);
-
-
+        $(".inputAddress").autocomplete({
+            source: address_list,
+            autoFocus: true,
+        });
     });
